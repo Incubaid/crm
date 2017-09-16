96e069c234fbc9b44562d6b0ca6d30f4878f02d4
Ahmed T. Youssef <xmonader@gmail.com>
2017-09-16 14:22:27 +0200

    cleanup formatters and customconverter for enums and removing owner/backupowner for deals

--- flaskcrm/views.py
+++ flaskcrm/views.py
@@ -4,46 +4,11 @@ import sqlalchemy
 import flask_admin
 from flask_admin.model import typefmt
 from flask_admin.base import expose
-from flask_admin.form.fields import Select2Field
-from flask_admin.model.form import converts
 from datetime import datetime
 from models import db, Telephone as TelephoneModel, Email as EmailModel, User as UserModel, Contact as ContactModel, Company as CompanyModel, Organization as OrganizationModel, Deal as DealModel, Deal as DealModel, Link as LinkModel, Project as ProjectModel, Sprint as SprintModel, Task as TaskModel, Comment as CommentModel, Message as MessageModel
-from formatters  import column_formatters
-class EnumField(Select2Field):
-    def __init__(self, column, **kwargs):
-        assert isinstance(column.type, sqlalchemy.sql.sqltypes.Enum)
+from formatters import column_formatters
+from converters import CustomAdminConverter
 
-        def coercer(value):
-            # coerce incoming value into an enum value
-            if isinstance(value, column.type.enum_class):
-                return value
-            elif isinstance(value, str):
-                return column.type.enum_class[value]
-            else:
-                assert False
-
-        super(EnumField, self).__init__(
-            choices=[(v, v) for v in column.type.enums],
-            coerce=coercer,
-            **kwargs)
-
-    def pre_validate(self, form):
-        # we need to override the default SelectField validation because it
-        # apparently tries to directly compare the field value with the choice
-        # key; it is not clear how that could ever work in cases where the
-        # values and choice keys must be different types
-
-        for (v, _) in self.choices:
-            if self.data == self.coerce(v):
-                break
-        else:
-            raise ValueError(self.gettext('Not a valid choice'))
-
-
-class CustomAdminConverter(flask_admin.contrib.sqla.form.AdminModelConverter):
-    @converts("sqlalchemy.sql.sqltypes.Enum")
-    def conv_enum(self, field_args, **extra):
-        return EnumField(column=extra["column"], **field_args)
 
 class EnhancedModelView(ModelView):
     can_view_details = True
@@ -243,7 +208,7 @@ class DealModelView(EnhancedModelView):
                   'comments',)
 
     form_edit_rules = ('name',  'amount', 'currency', 'deal_type', 'deal_state',
-                       'contact', 'company', 'owner', 'ownerbackup', 'tasks', 'messages', 'comments')
+                       'contact', 'company', 'tasks', 'messages', 'comments')
 
     column_list = ( 'name', 'amount', 'currency',
                    'deal_type', 'deal_state')
