de15f4fef5051865ee5c2873ed3a5cc396ef8fda
Ahmed T. Youssef <xmonader@gmail.com>
2017-10-01 12:10:40 +0200

    updates for app support

--- crm/middlewares.py
+++ crm/middlewares.py
@@ -25,47 +25,59 @@ def authenticate():
     so that g.user always hold current logged in user
     """
     jwt = request.cookies.get('caddyoauth')
-
     if jwt is None:
-        abort(401)
-
+        authheader = request.headers.get("Authorization", None)
+        if authheader is None or 'Bearer' not in authheader:
+            abort(401)
+        jwt = authheader.split(" ", 1)[1]  # Bearer JWT
     claims = get_unverified_claims(jwt)
-    username = claims.get('username')
 
-    if not username:
-        abort(401)
+    user = None
+    globalid = claims.get("globalid", None)
 
-    url = "https://itsyou.online/api/users/{}/info".format(
-        username
-    )
-
-    response = requests.get(
-        url,
-        headers={
-            'Authorization': 'bearer {}'.format(jwt)
-        }
-    )
-
-    info = response.json()
-    email = info['emailaddresses'][0]['emailaddress']
-    phone = info['phonenumbers'][0]['phonenumber']
-
-    users = User.query.filter(
-        User.emails.contains(email),
-        User.telephones.contains(phone)
-    )
-
-    if users.count():
-        user = users[0]
+    if globalid is not None:
+        orgid = claims
+        users = User.query.filter(
+            User.firstname == globalid
+        )
+        if users.count():
+            user = users[0]
+        else:
+            user = User(
+                firstname=globalid)
+            db.session.add(user)
     else:
+        username = claims.get("username", None)
+        if username is None:
+            abort(401)
+        url = "https://itsyou.online/api/users/{}/info".format(
+            username
+        )
 
-        user = User(
-            firstname=info['firstname'] or username,
-            lastname=info['lastname'] or username,
-            emails=email,
-            telephones=phone)
+        response = requests.get(
+            url,
+            headers={
+                'Authorization': 'Bearer {}'.format(jwt)
+            }
+        )
+        info = response.json()
+        email = info['emailaddresses'][0]['emailaddress']
+        phone = info['phonenumbers'][0]['phonenumber']
 
-        db.session.add(user)
-        db.session.commit()
+        users = User.query.filter(
+            User.emails.contains(email),
+            User.telephones.contains(phone)
+        )
+        if users.count():
+            user = users[0]
+        else:
+            user = User(
+                firstname=info['firstname'] or username,
+                lastname=info['lastname'] or username,
+                emails=email,
+                telephones=phone)
 
+            db.session.add(user)
+
+    db.session.commit()
     g.user = user
