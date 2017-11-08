132a4c2a83fe69a080ae19c2c0770c07761f1d14
xmonader <xmonader@gmail.com>
2017-11-08 09:02:11 +0200

    update settings to support mailer

--- crm/settings_dev.py
+++ crm/settings_dev.py
@@ -3,14 +3,19 @@ import os
 DEBUG = True
 Testing = True
 
-DB_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dev.db'))
+DB_PATH = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 'dev.db'))
 
-SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///%s' % DB_PATH)
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'SQLALCHEMY_DATABASE_URI', 'sqlite:///%s' % DB_PATH)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False
 SQLALCHEMY_RECORD_QUERIES = True
 
-DATA_DIR  = os.getenv('DATA_DIR', 'data')
+DATA_DIR = os.getenv('DATA_DIR', 'data')
 
 CACHE_BACKEND_URI = os.getenv('CACHE_BACKEND_URI', 'memory://')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', '')
+
+SUPPORT_EMAIL = 'thabeta@greenitglobe.com'
