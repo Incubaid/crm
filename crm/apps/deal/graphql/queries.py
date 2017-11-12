483a3178ffda6330d0e81f71d84f8521e5062d81
Hamdy <hamdy.a.farag@gmail.com>
2017-11-12 12:23:31 +0200

    updated deals graphql queries

--- crm/apps/deal/graphql/queries.py
+++ crm/apps/deal/graphql/queries.py
@@ -2,6 +2,8 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy.fields import SQLAlchemyConnectionField
 
+from crm.apps.deal.graphql.arguments import DealArguments
+from crm.apps.deal.models import Deal
 from .types import DealType
 from crm.graphql import BaseQuery
 
@@ -12,13 +14,27 @@ class DealQuery(BaseQuery):
     """
 
     # no need for resplve_Deals function here
-    deals = SQLAlchemyConnectionField(DealType)
+    deals = SQLAlchemyConnectionField(
+        DealType,
+        DealArguments.fields()
+
+    )
+
     # Deal query to return one Deal and takes (uid) argument
     # uid is the original object.id in db
     deal = graphene.Field(DealType, uid=graphene.String())
 
     def resolve_deal(self, context, uid):
         return DealType.get_query(context).filter_by(id=uid).first()
+
+    def resolve_deals(
+        self,
+        context,
+        *args,
+        **kwargs
+    ):
+
+        return BaseQuery.resolve_query(Deal, **kwargs)
 
     class Meta:
         interfaces = (relay.Node,)
