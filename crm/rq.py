2441452e71b996e85e7706b5e4fcd9b90b9a0afc
hamdy <hamdy.a.farag@gmail.com>
2018-01-09 05:02:13 +0200

    fix rq

--- crm/rq.py
+++ crm/rq.py
@@ -1,4 +1,13 @@
-from redis import Redis
+import os
+import redis
 from rq import Queue
 
-queue = Queue(connection=Redis())
+redis_url  = os.getenv('CACHE_BACKEND_URI')
+if redis_url:
+    redis_url = redis_url[:-2] + '/1'
+else:
+    redis_url = 'redis://localhost:6379/1'
+
+conn = redis.from_url(redis_url)
+
+queue = Queue(connection=conn)
