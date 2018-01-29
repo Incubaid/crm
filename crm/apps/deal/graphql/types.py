ac62c2b1942876a6e85ee0fef10ebe4cb3942e10
hamdy <hamdy.a.farag@gmail.com>
2018-01-29 12:25:53 +0200

    remove fund rounds

--- crm/apps/deal/graphql/types.py
+++ crm/apps/deal/graphql/types.py
@@ -18,37 +18,3 @@ class DealType(CrmType, SQLAlchemyObjectType):
             'author_original_id',
             'author_last_id'
         )
-
-
-class DealsStatsType(ObjectType):
-    start = DateTime()
-    end = DateTime()
-    total = graphene.Float()
-    ambassador = graphene.Float()
-    new = graphene.Float()
-    interested = graphene.Float()
-    confirmed = graphene.Float()
-    created = graphene.Float()
-    signed = graphene.Float()
-    paid = graphene.Float()
-    closed = graphene.Float()
-    hoster = graphene.Float()
-    ito = graphene.Float()
-    pto = graphene.Float()
-    itft = graphene.Float()
-    prepto = graphene.Float()
-    lost = graphene.Float()
-    target = graphene.Float()
-
-    class Meta:
-        interfaces = (relay.Node,)
-        name = 'dealsStats'
-
-
-class OverAllDealStatsType(ObjectType):
-    current_round = graphene.Field(DealsStatsType)
-    old_rounds = graphene.List(DealsStatsType)
-
-    class Meta:
-        interfaces = (relay.Node,)
-        name = 'OverAllDealStats'
